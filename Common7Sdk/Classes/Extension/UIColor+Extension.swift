15f20072364af7baf9d15b3e780cbc40181199f8
GigoGogo790 <gogopaly@163.com>
2023-02-24 09:34:05 +0800

    + 扩展

---
+++ Common7Sdk/Classes/Extension/UIColor+Extension.swift
@@ -0,0 +1,36 @@
+public extension UIColor {
+    
+    static func hexColor(_ value: String) -> UIColor {
+        return hexColor(value, alpha: 1)
+    }
+    
+    static func hexColor(_ value: String, alpha: CGFloat) -> UIColor {
+        
+        var r: CGFloat = 0
+        var g: CGFloat = 0
+        var b: CGFloat = 0
+        
+        var hex: String = value
+        if hex.hasPrefix("#") {
+            let index = hex.index(hex.startIndex, offsetBy: 1)
+            hex = String(hex[index...])
+        }
+        
+        let scanner = Scanner(string: hex)
+        var hexValue: CUnsignedLongLong = 0
+        
+        guard scanner.scanHexInt64(&hexValue) else {
+            return UIColor.clear
+        }
+        
+        if hex.count == 6 {
+            r = CGFloat((hexValue & 0xFF0000) >> 16) / 255.0
+            g = CGFloat((hexValue & 0x00FF00) >> 8) / 255.0
+            b = CGFloat(hexValue & 0x0000FF) / 255.0
+        } else {
+            return UIColor.clear
+        }
+        
+        return UIColor(red: r, green: g, blue: b, alpha: alpha)
+    }
+}
