cbf9704d60bdaf8b96eee1e81f12920487ba16b2
GigoGogo790 <gogopaly@163.com>
2023-03-15 15:16:13 +0800

    update .gitignore

--- Example/Pods/Target Support Files/Pods-Common7Sdk_Example/Pods-Common7Sdk_Example-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_Common7Sdk_Example : NSObject
-@end
-@implementation PodsDummy_Pods_Common7Sdk_Example
-@end
--- Example/Pods/Target Support Files/Pods-Common7Sdk_Tests/Pods-Common7Sdk_Tests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_Common7Sdk_Tests : NSObject
-@end
-@implementation PodsDummy_Pods_Common7Sdk_Tests
-@end
